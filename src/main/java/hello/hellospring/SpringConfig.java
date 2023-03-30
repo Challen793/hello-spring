c438c7a00255f79feaec1bc8345e8e49054bd41a
qkralstkd123Notebook <qkralstkd123@gmail.com>
2023-03-30 23:07:36 +0900

    ﻿스프링부트 - DB접근 기술: JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repositorry.JdbcMemberRepository;
 import hello.hellospring.repositorry.MemberRepository;
-import hello.hellospring.repositorry.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    //스프링부트가 application.properties를 읽고 만든 dataSource를 주입해주기
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource); //Jdbc연결로 변경
     }
 }
